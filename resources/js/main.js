f7994af204068820c240621ca006ed03fce6c999
Denicoli <tiagodenicoli@hotmail.com>
2025-05-21 01:39:37 -0300

    feat(icons): Add lucide-vue-next icons to the application

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router'
+import { User, Plus, Eye, Pencil, Trash } from 'lucide-vue-next'
 
 const app = createApp(App)
+
+const icons = { User, Plus, Eye, Pencil, Trash }
+
+Object.entries(icons).forEach(([name, component]) => {
+    app.component(`${name}Icon`, component)
+})
+
 app.use(router)
 app.mount('#app')
