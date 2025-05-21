abe466cd3b3fd8663ccfedf432b352596693e1b4
Denicoli <tiagodenicoli@hotmail.com>
2025-05-21 12:47:05 -0300

    fix(Login.vue): Update labels and button text for consistency and clarity

--- resources/js/Views/Login.vue
+++ resources/js/Views/Login.vue
@@ -1,38 +1,32 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="w-full max-w-md bg-white p-6 rounded shadow">
-      <h2 class="text-2xl font-bold mb-4 text-center">Login</h2>
+      <h2 class="text-2xl font-bold mb-4 text-center">Sign In</h2>
       
       <form @submit.prevent="handleLogin">
         <div class="mb-4">
           <label class="block text-sm font-medium mb-1">Email</label>
-          <input
-            v-model="form.email"
-            type="email"
-            class="w-full border px-3 py-2 rounded"
-            required
-          />
+          <input v-model="form.email" type="email" class="input" required />
         </div>
 
         <div class="mb-4">
-          <label class="block text-sm font-medium mb-1">Senha</label>
-          <input
-            v-model="form.password"
-            type="password"
-            class="w-full border px-3 py-2 rounded"
-            required
-          />
+          <label class="block text-sm font-medium mb-1">Password</label>
+          <input v-model="form.password" type="password" class="input" required />
         </div>
 
         <div v-if="error" class="mb-4 text-red-600 text-sm">{{ error }}</div>
 
         <button
           type="submit"
-          class="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+          class="w-full bg-blue-600 font-semibold text-white py-2 rounded hover:bg-blue-700"
         >
-          Entrar
+          Login
         </button>
       </form>
+      <div class="mt-4 text-sm font-normal">
+          Don't have an account? 
+          <router-link to="/register" class="text-blue-600 hover:underline">Register here</router-link>
+        </div>
     </div>
   </div>
 </template>
@@ -56,15 +50,22 @@ export default {
       try {
         const response = await api.post('/login', this.form)
 
-        // Armazena o token (ex: localStorage, pinia, etc.)
         localStorage.setItem('token', response.data.token)
 
-        // Redireciona pro dashboard
         this.$router.push('/')
       } catch (err) {
-        this.error = err.response?.data?.message || 'Erro ao fazer login.'
+        this.error = err.response?.data?.message || 'Login failed'
       }
     },
   },
 }
 </script>
+
+<style scoped>
+  .input {
+  @apply w-full border px-3 py-2 rounded;
+  }
+  .btn {
+    @apply w-full py-2 bg-blue-600 text-white rounded hover:bg-blue-700;
+  }
+</style>
