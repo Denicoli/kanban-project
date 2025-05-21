9ed314d7e415dde54a69c7bafd0a0833241bbe77
Denicoli <tiagodenicoli@hotmail.com>
2025-05-21 01:38:09 -0300

    refactor(api): Move registration and login routes inside auth middleware group

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,14 @@ use App\Http\Controllers\BoardController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ColumnController;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('boards', BoardController::class);
     Route::apiResource('boards.columns', ColumnController::class)->shallow();
     Route::apiResource('columns.tasks', TaskController::class)->shallow();
-});
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
