8e3928e10a86070d74cf70bbf7b5e2fac3b34372
Denicoli <tiagodenicoli@hotmail.com>
2025-05-23 19:07:57 -0300

    feat(main.js): Add additional icons UI components

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router'
-import { User, Plus, Eye, Pencil, Trash } from 'lucide-vue-next'
+
+import {
+    User,
+    Plus,
+    Eye,
+    Pencil,
+    Trash,
+    Calendar,
+    ChevronLeft,
+    EllipsisVertical,
+    Ellipsis,
+} from 'lucide-vue-next'
 
 const app = createApp(App)
 
-const icons = { User, Plus, Eye, Pencil, Trash }
+const icons = {
+    User,
+    Plus,
+    Eye,
+    Pencil,
+    Trash,
+    Calendar,
+    ChevronLeft,
+    EllipsisVertical,
+    Ellipsis,
+}
 
-Object.entries(icons).forEach(([name, component]) => {
+for (const [name, component] of Object.entries(icons)) {
     app.component(`${name}Icon`, component)
-})
+}
 
 app.use(router)
 app.mount('#app')
