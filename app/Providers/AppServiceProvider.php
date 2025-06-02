99a2529d043f7951d943b6493fa9c0b8a391687a
Denicoli <tiagodenicoli@hotmail.com>
2025-06-02 11:35:26 -0300

    feat(AppServiceProvider.php): Enforce HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use App\Policies\BoardPolicy;
 use App\Policies\ColumnPolicy;
 use App\Policies\TaskPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         Vite::prefetch(concurrency: 3);
         Gate::policy(Board::class, BoardPolicy::class);
         Gate::policy(Column::class, ColumnPolicy::class);
