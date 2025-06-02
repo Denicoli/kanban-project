a7d1f1ea91d0b5f1dbcd575f576defa460545c8e
Denicoli <tiagodenicoli@hotmail.com>
2025-06-02 11:54:11 -0300

    feat(render-build.sh): Add build script for automated setup and deployment

---
+++ render-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+composer install --no-dev --optimize-autoloader
+npm install
+npm run build
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan migrate --force
